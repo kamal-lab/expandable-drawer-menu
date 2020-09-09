94e8f57c22cc422b30aabde8a9624206cd1ca0b5
Kamal <kamalec03@gmail.com>
2020-09-09 17:56:32 +0530

    Added missed files into git.

---
+++ app/src/main/java/com/kamal/expandablemenu/MainActivity.java
@@ -0,0 +1,138 @@
+package com.kamal.expandablemenu;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.widget.ExpandableListView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    ExpandableListAdapter expandableListAdapter;
+    ExpandableListView expandableListView;
+    List<MenuModel> headerList = new ArrayList<>();
+    HashMap<MenuModel, List<MenuModel>> childList = new HashMap<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        // Init
+        expandableListView = findViewById(R.id.expandableListView);
+        prepareMenuData();
+        populateExpandableList();
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+    }
+
+    /**
+     * Add some menu into list
+     */
+    private void prepareMenuData() {
+        MenuModel menuModel = new MenuModel("Dashboard", true, false);
+        headerList.add(menuModel);
+
+        if (menuModel.hasChildren) {
+            childList.put(menuModel, null);
+        }
+
+        menuModel = new MenuModel("POS", true, true);
+        headerList.add(menuModel);
+
+        List<MenuModel> childModelList = new ArrayList<>();
+        MenuModel childModel = new MenuModel("Sale", false, false);
+        childModelList.add(childModel);
+
+        childModel = new MenuModel("Sale History", false, false);
+        childModelList.add(childModel);
+
+        childModel = new MenuModel("Cash Register", false, false);
+        childModelList.add(childModel);
+
+        if (menuModel.hasChildren) {
+            childList.put(menuModel, childModelList);
+        }
+    }
+
+    /**
+     * Set list view, and add listeners for header and child items
+     */
+    private void populateExpandableList() {
+        expandableListAdapter = new ExpandableListAdapter(this, headerList, childList);
+        expandableListView.setAdapter(expandableListAdapter);
+
+        expandableListView.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
+            @Override
+            public boolean onGroupClick(ExpandableListView expandableListView, View view, int groupPosition, long l) {
+                Log.d("Group click", headerList.get(groupPosition).menuName + " clicked");
+                if (!headerList.get(groupPosition).hasChildren) {
+                    onBackPressed();
+                }
+
+                return false;
+            }
+        });
+
+        expandableListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+            @Override
+            public boolean onChildClick(ExpandableListView expandableListView, View view, int groupPosition, int childPosition, long l) {
+                if (childList.get(headerList.get(groupPosition)) != null) {
+                    MenuModel menuModel = childList.get(headerList.get(groupPosition)).get(childPosition);
+                    Log.d("Child click", menuModel.menuName + " clicked");
+                    onBackPressed();
+                }
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+}
